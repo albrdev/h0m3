e5a0e4e7cc94f38108c047338feee6b843051deb
albrdev <albrdev@gmail.com>
2019-01-25 19:50:46 +0100

    init

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    // Awake is called once, before Start()
+    private void Awake()
+    {
+    }
+
+    // Start is called once per scene, before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
