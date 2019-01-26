b8d1c7addaf3abedf9e70e12bbc484e6948ce7c6
albrdev <albrdev@gmail.com>
2019-01-26 15:12:03 +0100

    rotator

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    protected Vector3 m_Rotation = Vector3.zero;
+
+    protected virtual void Update()
+    {
+        transform.Rotate(m_Rotation * UnityEngine.Time.deltaTime);
+    }
+}
