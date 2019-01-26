c77e84b5e07e02f99a06de8e3ac60bb6db3261a7
GaryAu83GitHub <gary.au.83.ga@gmail.com>
2019-01-26 12:03:54 +0100

    PlayerScript update
    The player can move with the left and right arriow button with very bad
    position.
    
    Collision box component has been implanted to the two object and a
    Rigidbody to the player.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	protected Rigidbody2D rgdb;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        rgdb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector2 direction = new Vector2();
+		
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+				direction.x = -1;
+		}
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+		{
+				direction.x = 1;
+		}
+
+		Move(direction);
+    }
+	
+	void Move(Vector2 aDir)
+	{
+		rgdb.position = rgdb.position + aDir;
+	}
+}
