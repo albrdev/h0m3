96ee8b175e8b95adf545bc63c4c4ef55bbba5187
albrdev <albrdev@gmail.com>
2019-01-26 19:32:28 +0100

    game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; } = null;
+
+    [SerializeField]
+    Transform m_BSOD;
+
+    public void OnPlayerSpawn()
+    {
+        m_BSOD.gameObject.SetActive(false);
+    }
+
+    public void OnPlayerDeath()
+    {
+        m_BSOD.gameObject.SetActive(true);
+    }
+
+    private void Awake()
+    {
+        if(Instance != null)
+            throw new System.Exception("Multiple objects of this type is not allowed");
+
+        Instance = this;
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+    }
+
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
+}
